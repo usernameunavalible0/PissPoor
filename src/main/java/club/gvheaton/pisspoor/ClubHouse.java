1fa1216a8d8fb79ad3be5458de2930eea6b3da36
Depressed Potato <77021141+usernameunavalible0@users.noreply.github.com>
2023-03-19 17:40:10 -0400

    beta 1

--- src/main/java/club/gvheaton/pisspoor/ClubHouse.java
+++ src/main/java/club/gvheaton/pisspoor/ClubHouse.java
@@ -1,5 +1,8 @@
 package club.gvheaton.pisspoor;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ClubHouse extends JavaPlugin {
@@ -13,5 +16,25 @@ public final class ClubHouse extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+    {
+        if (label.equalsIgnoreCase("fuckyou"))
+        {
+            if (sender instanceof Player)
+            {
+                Player player = (Player)sender;
+                player.sendMessage("Congrats!!! YOU found the *secret* command!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            }
+            else
+            {
+                sender.sendMessage("You cant use this command from console dumbass.");
+            }
+
+            return true;
+        }
+
+        return false;
     }
 }
